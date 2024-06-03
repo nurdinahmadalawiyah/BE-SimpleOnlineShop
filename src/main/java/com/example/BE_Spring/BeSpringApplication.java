670b617127cf3748b112dc05f3d8cd5a273304bd
nurdinalawiyah <nurdinahmada@gmail.com>
2024-06-03 14:17:19 +0700

    first commit

---
+++ src/main/java/com/example/BE_Spring/BeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.BE_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BE_Spring/BeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
